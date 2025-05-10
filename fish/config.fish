f4f33c4ca5f14466519291e8e3b8e35983c7ec5d
salim <lesalgeriensdesstars@gmail.com>
2025-05-10 17:10:23 +0100

    muehehehehe

---
+++ fish/config.fish
@@ -0,0 +1,22 @@
+set EDITOR  nvim
+set PAGER "nvim +Man!"
+set MANPAGER "nvim +Man!"
+set fish_greeting
+alias vi nvim
+
+alias cp 'cp -v'
+alias mv 'mv -v'
+
+alias python "~/.venv/bin/python"
+alias pip    "~/.venv/bin/pip"
+alias man 'man --pager="nvim +Man!"'
+
+export NEOCITIES_USER='pepelemoko'
+export NEOCITIES_PASS='5LbpZ^9RG;A"Gc%'
+
+neofetch
+
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
