392115de6a1d1654bb511791d65c77d4d31731fb
salim <salim.belkacemm@protonmail.com>
2026-03-17 14:28:33 +0100

    cleaned that mess

--- fish/config.fish
+++ fish/config.fish
@@ -1,32 +1,22 @@
-set EDITOR nvim set PAGER "nvim +Man!"
-set MANPAGER "nvim +Man!"  set
-set -U fish_greeting
-alias vi nvim
-
-alias cp 'cp -v'
-alias mv 'mv -v'
-
-# alias python "~/.venv/bin/python" alias pip "~/.venv/bin/pip" alias
-# man 'man --pager="nvim +Man!"'
-
-# export NEOCITIES_USER='pepelemoko'
-# export NEOCITIES_PASS='5LbpZ^9RG;A"Gc%'
-
-
 if status is-interactive
 	fastfetch
-    # Commands to run in interactive sessions can go here
 end
 
-set -x PATH /home/salim_belkacem/go/bin $PATH
-set -x PATH /home/salim_belkacem/.local/share/gem/ruby/3.4.0/bin $PATH
+alias vi nvim
+alias cp 'cp -v'
+alias mv 'mv -v'
+alias man 'man --pager="nvim +Man!"'
 
-set -x PATH "$HOME/.bun/bin" $PATH
-set -x PATH "$HOME/.cargo/bin" $PATH
-set -x PATH "$HOME/.venv/bin" $PATH 
+set -U fish_greeting
 
-set -x PATH "/usr/lib/emsdk" $PATH 
-set -x PATH "/usr/lib/emsdk/upstream/emscripten" $PATH
+set -x PATH "$HOME/go/bin" \
+"$HOME/.local/share/gem/ruby/3.4.0/bin" \
+"$HOME/.local/bin/" \
+"$HOME/.bun/bin" \
+"$HOME/.cargo/bin" \
+"$HOME/.venv/bin" \
+"/usr/lib/emsdk" \
+"/usr/lib/emsdk/upstream/emscripten" $PATH
     
     
 fish_vi_key_bindings
