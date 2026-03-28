029298020c6373c028d209f28e98d473336be8b4
salim <salim.belkacemm@protonmail.com>
2026-03-28 16:47:24 +0100

    forgot whatr i did ^^

--- fish/config.fish
+++ fish/config.fish
@@ -6,17 +6,20 @@ alias vi nvim
 alias cp 'cp -v'
 alias mv 'mv -v'
 alias man 'man --pager="nvim +Man!"'
+alias yay 'yay --noconfirm'
+alias neofetch fastfetch
+alias sudo sl
 
-set -U fish_greeting
-
-set -x PATH "$HOME/go/bin" \
+set -x PATH 				\
+"$HOME/go/bin" 				\
 "$HOME/.local/share/gem/ruby/3.4.0/bin" \
-"$HOME/.local/bin/" \
-"$HOME/.bun/bin" \
-"$HOME/.cargo/bin" \
-"$HOME/.venv/bin" \
-"/usr/lib/emsdk" \
-"/usr/lib/emsdk/upstream/emscripten" $PATH
+"$HOME/.local/bin/" 			\
+"$HOME/.bun/bin" 			\
+"$HOME/.cargo/bin" 			\
+"$HOME/.venv/bin" 			\
+"/usr/lib/emsdk" 			\
+"/usr/lib/emsdk/upstream/emscripten"	$PATH
     
-    
+set -gx QT_QPA_PLATFORMTHEME qt6ct
+
 fish_vi_key_bindings
